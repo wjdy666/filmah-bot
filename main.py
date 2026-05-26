cef6bb881d2c3a95a8c45ad43fc43ca8f900afe2
wjdy666 <60191793+wjdy666@users.noreply.github.com>
2026-05-26 12:30:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import os
 import logging
-import requests
+import request
+import threading
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Bot is alive and running!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+# تشغيل السيرفر الوهمي في الخلفية
+threading.Thread(target=run_web, daemon=True).start()
+ 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler
 
